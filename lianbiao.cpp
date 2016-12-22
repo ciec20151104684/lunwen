ed12978d60f711c8461142e183147d802b448895
ciec20151104684 <ciec20151104684@users.noreply.github.com>
2016-12-22 20:14:57 +0800

    Add files via upload

---
+++ lianbiao.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#define N 1
+    struct Node
+    {
+        char name[40];
+        int age;
+        struct Node*next;
+    };
+    int main()
+    {
+        struct Node*head,*p,*tail;
+        int i;
+        
+        FILE *fp;
+        char ch;
+        //char *p=NULL;
+        fp=fopen("test.txt","r");
+        if(fp==NULL)
+        {
+        	printf("文件打不开\n");
+        	exit(-1);
+		}
+		else
+		{
+			ch=fgetc(fp);
+			while(ch!=EOF)
+			{
+				printf("%c",ch);
+				ch=fgetc(fp);
+			}
+			fclose(fp);
+		}
+        
+        
+        head=new Node;
+        head->next=0;
+        tail=head;
+        //scanf("%s",head->name);
+        //scanf("%d",&head->age);
+        for(i=0;i<N;i++)
+        {
+            p=new Node;
+            scanf("%s",p->name);
+            scanf("%d",&p->age);
+            p->next=0;
+            tail->next=p;
+            tail=p;
+        }
+        p=head;
+        while(p)
+        {
+            printf("%s %d\n",p->name,p->age);
+            p=p->next;
+        }
+        return 0;
+    }
